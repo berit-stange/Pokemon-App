d9ad2661acd5cdb2735b9ed734ee4a645deed2dc
berit-stange <berit-stange@users.noreply.github.com>
2021-01-25 23:14:38 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/* This is a longer comment.
+It might explain, in detail, why we made a certain decision.
+Or, it could show examples about how to use the following code snippet. */
+
+// RECOMMENDED
+alert('Hello world');
+
+let favoriteFood = 'Pizza';
+document.write(favoriteFood);
