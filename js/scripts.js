de3a891312d2af1bd981088c3959d7ab83c64a9d
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-02-10 17:28:35 +0100

    deleted unused code

--- js/scripts.js
+++ js/scripts.js
@@ -23,27 +23,6 @@ let pokemonList = [
 ];
 
 
-// PART 1: create a for loop that iterates over each item in pokemonList
-
-//for (let i=0; i < pokemonList.length; i++){
-//    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ", " + " types: " + pokemonList[i].types + ") " + "</br>");
-//  }
-
-
-// PART 3: adding a conditional for height
-//  for (let i=0; i < pokemonList.length; i++){
-//    if (pokemonList[i].height <0.6){
-//    document.write('<p  class="list__item">' + pokemonList[i].name + "<br>" + "height: " + pokemonList[i].height + "<br>" + "types: " + pokemonList[i].types + "<br>" + " - That's a small one </p>");
-//  } 
-//  else if (pokemonList[i].height <1){
-//    document.write('<p  class="list__item">' + pokemonList[i].name + "<br>" + "height: " + pokemonList[i].height + "<br>" + "types: " + pokemonList[i].types + "<br>" + " - Kinda medium I guess </p>");
-//  } 
-//  else {
-//    document.write('<p  class="list__item">' + pokemonList[i].name + "<br>" + "height: " + pokemonList[i].height + "<br>" + "types: " + pokemonList[i].types + "<br>" + " - Wow, that’s big! </p>");
-//  }
-//  }
-
-
 // TASK 1.5 --- PART 1: forEach loop
 pokemonList.forEach(function(pokemon) {
     if (pokemon.height <0.5){
