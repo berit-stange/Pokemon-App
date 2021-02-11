e902fb23c680e1c11db0269d122c3c91945f653f
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-02-11 21:37:06 +0100

    created pokemonRepository variable, added forEach() loop; updated loop code
    created pokemonRepository variable to hold what IIFE will return;
    added IIFE return;
    forEach() loop outside the IIFE;
    updated loop code

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,57 @@
 // array of objects
-let pokemonList = [
-  { 
-   name: 'Butterfree', 
-   height: 1.1, 
-   types: ['grass', 'flying', 'rock'],
-  },
-  { 
-   name: 'Jigglypuff', 
-   height: 0.5, 
-   types: ['poison', 'steel'],
-  },
-  { 
-   name: 'Weedle', 
-   height: 0.3, 
-   types: ['fairy', 'grass', 'poison'],
-  },
-  { 
-   name: 'Vulpix', 
-   height: 0.6, 
-   types: ['fairy', 'water', 'ice'],
-  }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+      { 
+        name: 'Butterfree', 
+        height: 1.1, 
+        types: ['grass', 'flying', 'rock'],
+      },
+      { 
+        name: 'Jigglypuff', 
+        height: 0.5, 
+        types: ['poison', 'steel'],
+      },
+      { 
+        name: 'Weedle', 
+        height: 0.3, 
+        types: ['fairy', 'grass', 'poison'],
+      },
+      { 
+        name: 'Vulpix', 
+        height: 0.6, 
+        types: ['fairy', 'water', 'ice'],
+      }
+    ];
+    
+    function add(item) {
+        pokemonList.push(item);
+    }
+    function getAll() {
+        return pokemonList;
+    }
+    return {
+        add: add,
+        getAll: getAll
+  };
+})();
+
+    let item =  {
+        name: 'Pikachu',
+        height: 2, 
+        types:'water'
+    }
+pokemonRepository.add(item);  
 
 
-// TASK 1.5 --- PART 1: forEach loop
-pokemonList.forEach(function(pokemon) {
-    if (pokemon.height <0.5){
+// TASK 1.5 --- PART 2: forEach loop
+pokemonRepository.getAll().forEach(function (pokemon) {
+    if (pokemon.height <=0.6){
     document.write('<p  class="list__item">' + pokemon.name + "<br>" + "height: " + pokemon.height + "<br>" + "types: " + pokemon.types + "<br>" + " - That's a small one </p>");
   } 
-  else if (pokemon.height <0.6){
-    document.write('<p  class="list__item">' + pokemon.name + "<br>" + "height: " + pokemon.height + "<br>" + "types: " + pokemon.types + "<br>" + " - Kinda medium I guess </p>");
+  else if (pokemon.height >0.6){
+    document.write('<p  class="list__item">' + pokemon.name + "<br>" + "height: " + pokemon.height + "<br>" + "types: " + pokemon.types + "<br>" + " - Kinda big </p>");
   } 
   else {
-    document.write('<p  class="list__item">' + pokemon.name + "<br>" + "height: " + pokemon.height + "<br>" + "types: " + pokemon.types + "<br>" + " - Wow, that’s big! </p>");
+    document.write('<p  class="list__item">' + pokemon.name + "<br>" + "height: " + pokemon.height + "<br>" + "types: " + pokemon.types + "<br>" + " - height missing </p>");
   }
-  })
+  });
